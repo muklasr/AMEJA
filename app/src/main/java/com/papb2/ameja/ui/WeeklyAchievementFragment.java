bfeeadd95573a7379232b48938320975fad058d1
Muklas Rahmanto <muklas.r@mail.ugm.ac.id>
2020-04-22 20:07:48 +0700

    add chart and show switch chart dialog

--- app/src/main/java/com/papb2/ameja/ui/WeeklyAchievementFragment.java
+++ app/src/main/java/com/papb2/ameja/ui/WeeklyAchievementFragment.java
@@ -1,29 +1,154 @@
 package com.papb2.ameja.ui;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.chart.common.listener.Event;
+import com.anychart.chart.common.listener.ListenersInterface;
+import com.anychart.charts.Cartesian;
+import com.anychart.charts.Pie;
+import com.anychart.core.cartesian.series.Line;
+import com.anychart.data.Mapping;
+import com.anychart.data.Set;
+import com.anychart.enums.Align;
+import com.anychart.enums.Anchor;
+import com.anychart.enums.LegendLayout;
+import com.anychart.enums.MarkerType;
+import com.anychart.enums.TooltipPositionMode;
+import com.anychart.graphics.vector.Stroke;
+import com.papb2.ameja.MainActivity;
 import com.papb2.ameja.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class WeeklyAchievementFragment extends Fragment {
+public class WeeklyAchievementFragment extends Fragment implements View.OnClickListener {
 
-    public WeeklyAchievementFragment() {
-        // Required empty public constructor
-    }
-
+    private AnyChartView anyChartView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_weekly_achievement, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        anyChartView = view.findViewById(R.id.any_chart_view);
+        Button btnMode = view.findViewById(R.id.btnMode);
+
+        setupChart(view);
+
+        btnMode.setOnClickListener(this);
+    }
+
+    private void setupChart(View view) {
+        anyChartView.setProgressBar(view.findViewById(R.id.progress_bar));
+
+        Cartesian cartesian = AnyChart.line();
+
+        cartesian.animation(true);
+
+        cartesian.padding(10d, 20d, 5d, 20d);
+
+        cartesian.crosshair().enabled(true);
+        cartesian.crosshair()
+                .yLabel(true)
+                // TODO ystroke
+                .yStroke((Stroke) null, null, null, (String) null, (String) null);
+
+        cartesian.tooltip().positionMode(TooltipPositionMode.POINT);
+
+        cartesian.title("Trend of Sales of the Most Popular Products of ACME Corp.");
+
+        cartesian.yAxis(0);
+//        cartesian.yAxis(0).title("Number of Bottles Sold (thousands)");
+        cartesian.xAxis(0).labels().padding(5d, 5d, 5d, 5d);
+
+        List<DataEntry> seriesData = new ArrayList<>();
+        seriesData.add(new CustomDataEntry("7 April", 10, 2));
+        seriesData.add(new CustomDataEntry("8 April", 12, 0));
+        seriesData.add(new CustomDataEntry("9 April", 12, 1));
+        seriesData.add(new CustomDataEntry("10 April", 10, 0));
+        seriesData.add(new CustomDataEntry("11 April", 14, 3));
+        seriesData.add(new CustomDataEntry("12 April", 12, 2));
+        seriesData.add(new CustomDataEntry("Today", 12, 3));
+
+        Set set = Set.instantiate();
+        set.data(seriesData);
+        Mapping series1Mapping = set.mapAs("{ x: 'x', value: 'value' }");
+        Mapping series2Mapping = set.mapAs("{ x: 'x', value: 'value2' }");
+
+        Line series1 = cartesian.line(series1Mapping);
+        series1.name("Completed");
+        series1.hovered().markers().enabled(true);
+        series1.hovered().markers()
+                .type(MarkerType.CIRCLE)
+                .size(4d);
+        series1.tooltip()
+                .position("right")
+                .anchor(Anchor.LEFT_CENTER)
+                .offsetX(5d)
+                .offsetY(5d);
+
+        Line series2 = cartesian.line(series2Mapping);
+        series2.name("Not Completed");
+        series2.hovered().markers().enabled(true);
+        series2.hovered().markers()
+                .type(MarkerType.CIRCLE)
+                .size(4d);
+        series2.tooltip()
+                .position("right")
+                .anchor(Anchor.LEFT_CENTER)
+                .offsetX(5d)
+                .offsetY(5d);
+
+        cartesian.legend().enabled(true);
+        cartesian.legend().fontSize(13d);
+        cartesian.legend().padding(0d, 0d, 10d, 0d);
+
+        anyChartView.setChart(cartesian);
+    }
+
+    private void showDialog() {
+        ChartDialogFragment chartDialogFragment = new ChartDialogFragment((AchievementFragment) getParentFragment());
+        chartDialogFragment.show(getChildFragmentManager(), "Dialog");
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnMode:
+                showDialog();
+                break;
+        }
+    }
+
+    private class CustomDataEntry extends ValueDataEntry {
+
+        CustomDataEntry(String x, Number value, Number value2) {
+            super(x, value);
+            setValue("value2", value2);
+        }
+
     }
 }
