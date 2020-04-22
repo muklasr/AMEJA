2cb7e0c51f0087ce3f7650a265338bd9eb563cf3
Muklas Rahmanto <muklas.r@mail.ugm.ac.id>
2020-04-22 20:07:57 +0700

    add chart and show switch chart dialog

--- app/src/main/java/com/papb2/ameja/ui/MonthlyAchievementFragment.java
+++ app/src/main/java/com/papb2/ameja/ui/MonthlyAchievementFragment.java
@@ -1,28 +1,99 @@
 package com.papb2.ameja.ui;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.chart.common.listener.Event;
+import com.anychart.chart.common.listener.ListenersInterface;
+import com.anychart.charts.Pie;
+import com.anychart.enums.Align;
+import com.anychart.enums.LegendLayout;
 import com.papb2.ameja.R;
 
-/**
- * A simple {@link Fragment} subclass.
- *
- */
-public class MonthlyAchievementFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
 
-    public MonthlyAchievementFragment() {
-        // Required empty public constructor
-    }
+public class MonthlyAchievementFragment extends Fragment implements View.OnClickListener  {
 
+    private AnyChartView anyChartView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_monthly_achievement, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        anyChartView = view.findViewById(R.id.any_chart_view);
+        Button btnMode = view.findViewById(R.id.btnMode);
+
+        setupChart(view);
+
+        btnMode.setOnClickListener(this);
+    }
+
+    private void setupChart(View view) {
+        anyChartView.setProgressBar(view.findViewById(R.id.progress_bar));
+
+        Pie pie = AnyChart.pie();
+
+        pie.setOnClickListener(new ListenersInterface.OnClickListener(new String[]{"x", "value"}) {
+            @Override
+            public void onClick(Event event) {
+                Toast.makeText(getActivity(), event.getData().get("x") + ":" + event.getData().get("value"), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        List<DataEntry> data = new ArrayList<>();
+        data.add(new ValueDataEntry("Completed", 54));
+        data.add(new ValueDataEntry("Not Completed", 6));
+
+        pie.data(data);
+
+        pie.title("Fruits imported in 2015 (in kg)");
+
+        pie.labels().position("outside");
+
+        pie.legend().title().enabled(true);
+        pie.legend().title()
+                .text("Retail channels")
+                .padding(0d, 0d, 10d, 0d);
+
+        pie.legend()
+                .position("center-bottom")
+                .itemsLayout(LegendLayout.HORIZONTAL)
+                .align(Align.CENTER);
+
+        anyChartView.setChart(pie);
+    }
+
+    private void showDialog() {
+        ChartDialogFragment chartDialogFragment = new ChartDialogFragment((AchievementFragment) getParentFragment());
+        chartDialogFragment.show(getChildFragmentManager(), "Dialog");
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnMode:
+                showDialog();
+                break;
+        }
     }
 }
