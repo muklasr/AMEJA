e39190d4a8b0dc31370504eece27962a1077a07f
Muklas Rahmanto <muklas.r@mail.ugm.ac.id>
2020-04-22 20:09:13 +0700

    move code to WeeklyAchievement

--- app/src/main/java/com/papb2/ameja/ui/AchievementFragment.java
+++ app/src/main/java/com/papb2/ameja/ui/AchievementFragment.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.anychart.AnyChart;
 import com.anychart.AnyChartView;
@@ -24,49 +26,25 @@ import com.papb2.ameja.R;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AcheivementFragment extends Fragment {
+public class AchievementFragment extends Fragment implements ChartDialogFragment.ClickCallback{
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_acheivement, container, false);
-        return root;
+        return inflater.inflate(R.layout.fragment_achievement, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        AnyChartView anyChartView = view.findViewById(R.id.any_chart_view);
-        anyChartView.setProgressBar(view.findViewById(R.id.progress_bar));
 
-        Pie pie = AnyChart.pie();
-
-        pie.setOnClickListener(new ListenersInterface.OnClickListener(new String[]{"x", "value"}) {
-            @Override
-            public void onClick(Event event) {
-                Toast.makeText(getActivity(), event.getData().get("x") + ":" + event.getData().get("value"), Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        List<DataEntry> data = new ArrayList<>();
-        data.add(new ValueDataEntry("Completed", 54));
-        data.add(new ValueDataEntry("Not Completed", 6));
-
-        pie.data(data);
-
-        pie.title("Fruits imported in 2015 (in kg)");
-
-        pie.labels().position("outside");
-
-        pie.legend().title().enabled(true);
-        pie.legend().title()
-                .text("Retail channels")
-                .padding(0d, 0d, 10d, 0d);
-
-        pie.legend()
-                .position("center-bottom")
-                .itemsLayout(LegendLayout.HORIZONTAL)
-                .align(Align.CENTER);
-
-        anyChartView.setChart(pie);
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, new MonthlyAchievementFragment()).addToBackStack(null).commit();
     }
+
+    @Override
+    public void onItemClicked(Fragment fragment) {
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment).addToBackStack(null).commit();
+    }
+
 }
