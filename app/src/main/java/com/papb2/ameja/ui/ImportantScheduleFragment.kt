8b94448af275d5821f7208e2bb19ab9cd09572ca
Muklas Rahmanto <muklas.r@mail.ugm.ac.id>
2020-04-29 07:07:02 +0700

    add loadData function

--- app/src/main/java/com/papb2/ameja/ui/ImportantScheduleFragment.kt
+++ app/src/main/java/com/papb2/ameja/ui/ImportantScheduleFragment.kt
@@ -1,7 +1,7 @@
 package com.papb2.ameja.ui
 
-import android.os.AsyncTask
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,15 +13,24 @@ import com.papb2.ameja.R
 import com.papb2.ameja.adapter.ImportantAdapter
 import com.papb2.ameja.db.ScheduleHelper
 import com.papb2.ameja.model.Schedule
-import java.lang.ref.WeakReference
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 import java.util.*
 
 /**
  * A simple [Fragment] subclass.
  */
-class ImportantScheduleFragment : Fragment(), LoadScheduleCallback {
-    private var scheduleHelper: ScheduleHelper? = null
-    private var adapter: ImportantAdapter? = null
+class ImportantScheduleFragment : Fragment() {
+
+    private lateinit var scheduleHelper: ScheduleHelper
+    private lateinit var adapter: ImportantAdapter
+
+    companion object {
+        private const val EXTRA_STATE = "EXTRA_STATE"
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? { // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_important_schedule, container, false)
@@ -31,65 +40,40 @@ class ImportantScheduleFragment : Fragment(), LoadScheduleCallback {
         super.onViewCreated(view, savedInstanceState)
         val rvSchedule: RecyclerView = view.findViewById(R.id.rvSchedule)
         scheduleHelper = ScheduleHelper(context!!)
-        scheduleHelper!!.open()
+        scheduleHelper.open()
         adapter = ImportantAdapter()
+
+        if (savedInstanceState == null) {
+            loadSchedulesAsync()
+        } else {
+            val list = savedInstanceState.getParcelableArrayList<Schedule>(EXTRA_STATE)
+            if (list != null) {
+                adapter.listSchedule = list
+            }
+        }
         rvSchedule.layoutManager = LinearLayoutManager(context)
         rvSchedule.setHasFixedSize(true)
         rvSchedule.adapter = adapter
-        if (savedInstanceState == null) { // proses ambil data
-            LoadScheduleAsync(scheduleHelper!!, context as LoadScheduleCallback?).execute()
-        } else {
-            val list: ArrayList<out Schedule?> = savedInstanceState.getParcelableArrayList(EXTRA_STATE)
-            if (list != null) {
-                adapter!!.setData(list as ArrayList<Schedule?>)
+    }
+    private fun loadSchedulesAsync() {
+        GlobalScope.launch(Dispatchers.Main) {
+            val deferredSchedules = async(Dispatchers.IO) {
+                val cursor = scheduleHelper.queryAll()
+                mapCursorToArrayList(cursor)
+            }
+
+            val schedules = deferredSchedules.await()
+            if (schedules.size > 0) {
+                adapter.setData(schedules)
+                Log.d("TAG", "Size ${schedules.size}")
+            } else {
+                adapter.listSchedule = ArrayList()
             }
         }
     }
 
-    protected override fun onSaveInstanceState(outState: Bundle) {
+    override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putParcelableArrayList(EXTRA_STATE, adapter!!.listSchedule)
+        outState.putParcelableArrayList(EXTRA_STATE, adapter.listSchedule)
     }
-
-    override fun preExecute() {}
-    override fun postExecute(schedules: ArrayList<Schedule?>) {
-        if (schedules.size > 0) {
-            adapter!!.setData(schedules)
-        } else {
-            adapter!!.setData(ArrayList())
-        }
-    }
-
-    private class LoadScheduleAsync(scheduleHelper: ScheduleHelper, callback: LoadScheduleCallback?) : AsyncTask<Void?, Void?, ArrayList<Schedule?>>() {
-        private val weakScheduleHelper: WeakReference<ScheduleHelper>
-        private val weakCallback: WeakReference<LoadScheduleCallback?>
-        override fun onPreExecute() {
-            super.onPreExecute()
-            weakCallback.get()!!.preExecute()
-        }
-
-        protected override fun doInBackground(vararg voids: Void): ArrayList<Schedule?> {
-            val dataCursor = weakScheduleHelper.get()!!.queryAll()
-            return mapCursorToArrayList(dataCursor)
-        }
-
-        override fun onPostExecute(schedules: ArrayList<Schedule?>) {
-            super.onPostExecute(schedules)
-            weakCallback.get()!!.postExecute(schedules)
-        }
-
-        init {
-            weakScheduleHelper = WeakReference(scheduleHelper)
-            weakCallback = WeakReference(callback)
-        }
-    }
-
-    companion object {
-        private const val EXTRA_STATE = "EXTRA_STATE"
-    }
-}
-
-internal interface LoadScheduleCallback {
-    fun preExecute()
-    fun postExecute(schedules: ArrayList<Schedule?>)
 }
