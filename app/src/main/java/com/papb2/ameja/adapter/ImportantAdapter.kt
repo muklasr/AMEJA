6221723be15fd0854c0e594a82711468bf4dec51
Muklas Rahmanto <muklas.r@mail.ugm.ac.id>
2020-04-29 07:05:22 +0700

    first

--- app/src/main/java/com/papb2/ameja/adapter/ImportantAdapter.kt
+++ app/src/main/java/com/papb2/ameja/adapter/ImportantAdapter.kt
@@ -1,4 +1,56 @@
 package com.papb2.ameja.adapter
 
-class ImportantAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.papb2.ameja.R
+import com.papb2.ameja.model.Schedule
+import kotlinx.android.synthetic.main.item_important_agenda.view.*
+
+class ImportantAdapter :
+        RecyclerView.Adapter<ImportantAdapter.CardViewViewHolder>() {
+
+    var listSchedule = ArrayList<Schedule>()
+    private var onItemClickCallback: OnItemClickCallback? = null
+
+    fun setData(items: ArrayList<Schedule>) {
+        listSchedule.clear()
+        listSchedule.addAll(items)
+        notifyDataSetChanged()
+    }
+
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback
+    }
+
+    override fun onCreateViewHolder(
+            parent: ViewGroup,
+            viewType: Int
+    ): CardViewViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_important_agenda, parent, false)
+        return CardViewViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = listSchedule.size
+
+    override fun onBindViewHolder(holder: CardViewViewHolder, position: Int) {
+        holder.bind(listSchedule[position])
+    }
+
+    inner class CardViewViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(schedule: Schedule) {
+            with(itemView) {
+                tvAgenda.text = schedule.agenda
+                tvInfo.text = "${schedule.start}-${schedule.end} at ${schedule.location}"
+
+                itemView.setOnClickListener { onItemClickCallback?.onItemClicked(schedule) }
+            }
+
+        }
+    }
+
+    interface OnItemClickCallback {
+        fun onItemClicked(data: Schedule)
+    }
 }
