b03adbae3fd7f7fe8b7afb8251f45fa0f3be7f89
Muklas Rahmanto <muklas.r@mail.ugm.ac.id>
2020-04-29 07:04:16 +0700

    add save function

--- app/src/main/java/com/papb2/ameja/ui/AddDialogFragment.java
+++ app/src/main/java/com/papb2/ameja/ui/AddDialogFragment.java
@@ -1,21 +1,40 @@
 package com.papb2.ameja.ui;
 
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 import com.papb2.ameja.R;
+import com.papb2.ameja.db.ScheduleHelper;
 
 import java.util.Calendar;
 
-public class AddDialogFragment extends BottomSheetDialogFragment {
+import static com.papb2.ameja.db.DatabaseContract.ScheduleColumns.AGENDA;
+import static com.papb2.ameja.db.DatabaseContract.ScheduleColumns.DATE;
+import static com.papb2.ameja.db.DatabaseContract.ScheduleColumns.END;
+import static com.papb2.ameja.db.DatabaseContract.ScheduleColumns.IMPORTANT;
+import static com.papb2.ameja.db.DatabaseContract.ScheduleColumns.LOCATION;
+import static com.papb2.ameja.db.DatabaseContract.ScheduleColumns.START;
+import static com.papb2.ameja.db.DatabaseContract.ScheduleColumns.STATUS;
+
+public class AddDialogFragment extends BottomSheetDialogFragment implements View.OnClickListener {
+
+    private EditText etAgenda;
+    private EditText etStart;
+    private EditText etEnd;
+    private EditText etLocation;
+    private Button btnSave;
+    private Button btnCancel;
 
     private Calendar selectedTime;
 
@@ -32,13 +51,56 @@ public class AddDialogFragment extends BottomSheetDialogFragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        EditText etAgenda = view.findViewById(R.id.etAgenda);
-        EditText etStart = view.findViewById(R.id.etStart);
-        EditText etStop = view.findViewById(R.id.etEnd);
-        Button btnSave = view.findViewById(R.id.btnSave);
-        Button btnCancel = view.findViewById(R.id.btnCancel);
+        etAgenda = view.findViewById(R.id.etAgenda);
+        etStart = view.findViewById(R.id.etStart);
+        etEnd = view.findViewById(R.id.etEnd);
+        etLocation = view.findViewById(R.id.etLocation);
+        btnSave = view.findViewById(R.id.btnSave);
+        btnCancel = view.findViewById(R.id.btnCancel);
 
         etStart.setText(selectedTime.get(Calendar.HOUR_OF_DAY) + ":00");
+        btnSave.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnSave:
+                saveSchedule();
+                break;
+            case R.id.btnCancel:
+                this.dismiss();
+                break;
+        }
+    }
+
+    private void saveSchedule() {
+        String agenda = etAgenda.getText().toString();
+        String date = String.valueOf(selectedTime.get(Calendar.DATE));
+        String start = etStart.getText().toString();
+        String end = etEnd.getText().toString();
+        String location = etLocation.getText().toString();
+        int status = 0;
+        boolean important = false;
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(AGENDA, agenda);
+        contentValues.put(DATE, date);
+        contentValues.put(START, start);
+        contentValues.put(END, end);
+        contentValues.put(LOCATION, location);
+        contentValues.put(STATUS, status);
+        contentValues.put(IMPORTANT, important);
+
+        ScheduleHelper scheduleHelper = new ScheduleHelper(getContext());
+        scheduleHelper.open();
+         scheduleHelper.insert(contentValues);
+        Cursor cursor = scheduleHelper.queryAll();
+        if (cursor.getCount() > 0) {
+            Toast.makeText(getContext(), "Schedule successfully added", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getContext(), "Schedule failed to be added", Toast.LENGTH_LONG).show();
+        }
+        scheduleHelper.close();
+    }
 }
