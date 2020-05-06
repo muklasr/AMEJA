46b0bb2b0b331f121cc39523c4303c15708140d4
Muklas Rahmanto <muklas.r@mail.ugm.ac.id>
2020-05-06 12:00:56 +0700

    update selected schedule

--- app/src/main/java/com/papb2/ameja/ui/AddDialogFragment.java
+++ app/src/main/java/com/papb2/ameja/ui/AddDialogFragment.java
@@ -35,10 +35,23 @@ public class AddDialogFragment extends BottomSheetDialogFragment implements View
     private EditText etEnd;
     private EditText etLocation;
 
+    private ScheduleHelper scheduleHelper;
+
     private Calendar selectedTime;
+    private Long idSchedule;
+    private Boolean isUpdate;
+    private String scheduleDate;
+    private int scheduleStatus = 0;
+    private Boolean isImportant = false;
 
     AddDialogFragment(Calendar time) {
         this.selectedTime = time;
+        this.isUpdate = false;
+    }
+
+    AddDialogFragment(Long id) {
+        this.idSchedule = id;
+        this.isUpdate = true;
     }
 
     @Nullable
@@ -57,9 +70,34 @@ public class AddDialogFragment extends BottomSheetDialogFragment implements View
         Button btnSave = view.findViewById(R.id.btnSave);
         Button btnCancel = view.findViewById(R.id.btnCancel);
 
-        etStart.setText(selectedTime.get(Calendar.HOUR_OF_DAY) + ":00");
         btnSave.setOnClickListener(this);
         btnCancel.setOnClickListener(this);
+
+        scheduleHelper = new ScheduleHelper(Objects.requireNonNull(getContext()));
+
+        if (isUpdate)
+            loadData();
+        else
+            etStart.setText(selectedTime.get(Calendar.HOUR_OF_DAY) + ":00");
+    }
+
+    private void loadData() {
+        scheduleHelper.open();
+        Cursor cursor = scheduleHelper.queryById(this.idSchedule.toString());
+
+        if (cursor.getCount() > 0) {
+            for (int i = 0; i < cursor.getCount(); i++) {
+                cursor.moveToPosition(i);
+                etAgenda.setText(cursor.getString(cursor.getColumnIndex(AGENDA)));
+                etStart.setText(cursor.getString(cursor.getColumnIndex(START)));
+                etEnd.setText(cursor.getString(cursor.getColumnIndex(END)));
+                etLocation.setText(cursor.getString(cursor.getColumnIndex(LOCATION)));
+                this.scheduleDate = cursor.getString(cursor.getColumnIndex(DATE));
+                this.scheduleStatus = cursor.getInt(cursor.getColumnIndex(STATUS));
+                this.isImportant = Boolean.valueOf(cursor.getString(cursor.getColumnIndex(IMPORTANT)));
+            }
+        }
+        scheduleHelper.close();
     }
 
     @Override
@@ -76,12 +114,17 @@ public class AddDialogFragment extends BottomSheetDialogFragment implements View
 
     private void saveSchedule() {
         String agenda = etAgenda.getText().toString();
-        String month = String.valueOf(selectedTime.get(Calendar.MONTH)+1);
-        String date = selectedTime.get(Calendar.DATE)+"/"+month+"/"+selectedTime.get(Calendar.YEAR);
+        String date;
+        if (isUpdate) {
+            date = this.scheduleDate;
+        } else {
+            String month = String.valueOf(selectedTime.get(Calendar.MONTH) + 1);
+            date = selectedTime.get(Calendar.DATE) + "/" + month + "/" + selectedTime.get(Calendar.YEAR);
+        }
         String start = etStart.getText().toString();
         String end = etEnd.getText().toString();
         String location = etLocation.getText().toString();
-        int status = 0;
+        int status = this.scheduleStatus;
 
         ContentValues contentValues = new ContentValues();
         contentValues.put(AGENDA, agenda);
@@ -90,20 +133,33 @@ public class AddDialogFragment extends BottomSheetDialogFragment implements View
         contentValues.put(END, end);
         contentValues.put(LOCATION, location);
         contentValues.put(STATUS, status);
-        contentValues.put(IMPORTANT, false);
+        contentValues.put(IMPORTANT, isImportant);
 
-        ScheduleHelper scheduleHelper = new ScheduleHelper(Objects.requireNonNull(getContext()));
         scheduleHelper.open();
-         scheduleHelper.insert(contentValues);
-        Cursor cursor = scheduleHelper.queryAll();
-        if (cursor.getCount() > 0) {
-            Toast.makeText(getContext(), "Schedule successfully added", Toast.LENGTH_LONG).show();
+
+        if (isUpdate) {
+            int result = scheduleHelper.update(idSchedule.toString(), contentValues);
+
+            if (result > 0) {
+                Toast.makeText(getContext(), R.string.update_success, Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(getContext(), R.string.update_fail, Toast.LENGTH_LONG).show();
+            }
         } else {
-            Toast.makeText(getContext(), "Schedule failed to be added", Toast.LENGTH_LONG).show();
+            int currentCount = scheduleHelper.queryAll().getCount();
+            scheduleHelper.insert(contentValues);
+
+            Cursor cursor = scheduleHelper.queryAll();
+            if (cursor.getCount() > currentCount) {
+                Toast.makeText(getContext(), R.string.add_success, Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(getContext(), R.string.add_fail, Toast.LENGTH_LONG).show();
+            }
         }
+
         scheduleHelper.close();
         assert getParentFragment() != null;
-        ((WeeklyScheduleFragment)getParentFragment()).refreshCalendar();
+        ((WeeklyScheduleFragment) getParentFragment()).refreshCalendar();
         this.dismiss();
     }
 }
